b92aef8742c934f2b3a5aaa8c8bc28e30ba340f6
Denis Kopeykin <denis.k@razortheory.com>
2020-01-27 18:45:21 +0300

    use shortcat render

--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import BillBoard
 
 
 def index(request):
-    template = loader.get_template("bboard/index.html")
     billboards = BillBoard.objects.order_by("-published")
-    context = {"billboards": billboards}
-    return HttpResponse(template.render(context, request))
+    return render(request, "bboard/index.html", {"billboards": billboards})
