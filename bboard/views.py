bc159d3c0dd3cf05f680e39359b855a93c732964
Denis Kopeykin <denis.k@razortheory.com>
2020-01-27 18:29:03 +0300

    show billboards on index view

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+from .models import BillBoard
 
-def index(request) :
-    return HttpResponse("Здecь будет выведен список объявлений.")
+
+def index(request):
+    response = "Список объявлений\n\n\n"
+    for bill_board in BillBoard.objects.order_by("-published"):
+        response += f"{bill_board.title}\n{bill_board.content}\n\n"
+    return HttpResponse(response, content_type="text/plain; charset=utf-8")
