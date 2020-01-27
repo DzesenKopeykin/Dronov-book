0994d69b01df10d80e7983181bcb0e20ace143e6
Denis Kopeykin <denis.k@razortheory.com>
2020-01-27 18:59:13 +0300

    add BillBoard model to admin

--- bboard/models.py
+++ bboard/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class BillBoard(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name="Product")
+    content = models.TextField(null=True, blank=True, verbose_name="Description")
+    price = models.FloatField(null=True, blank=True, verbose_name="Price")
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="published")
+
+    class Meta:
+        verbose_name = "Ad"
+        verbose_name_plural = "Ads"
+        ordering = ["-published"]
