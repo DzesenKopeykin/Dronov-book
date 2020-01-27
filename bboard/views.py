479f809fb94d7901cff64edfb44ab0de1155e32f
Denis Kopeykin <denis.k@razortheory.com>
2020-01-27 18:42:18 +0300

    create and use index template

--- bboard/views.py
+++ bboard/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import BillBoard
 
 
 def index(request):
-    response = "Список объявлений\n\n\n"
-    for bill_board in BillBoard.objects.order_by("-published"):
-        response += f"{bill_board.title}\n{bill_board.content}\n\n"
-    return HttpResponse(response, content_type="text/plain; charset=utf-8")
+    template = loader.get_template("bboard/index.html")
+    billboards = BillBoard.objects.order_by("-published")
+    context = {"billboards": billboards}
+    return HttpResponse(template.render(context, request))
