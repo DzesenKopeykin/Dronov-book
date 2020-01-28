ddd26ca4748602fe56e577de6504a05fd3b50541
Denis Kopeykin <kopeykindzzesen@gmail.com>
2020-01-28 18:55:41 +0300

    add by_rubric view

--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
-from .models import Ad
+from .models import Ad, Rubric
 
 
 def index(request):
     ads = Ad.objects.all()
     return render(request, "bboard/index.html", {"ads": ads})
+
+
+def by_rubric(request, rubric_id):
+    ads = Ad.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    context = {"ads": ads, "rubrics": rubrics, "rubric_id": rubric_id}
+    return render(request, "bboard/by_rubric.html", context)
