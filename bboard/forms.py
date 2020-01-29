f1af49c04f4ab2f6125a739a2dc412631da125a8
Denis Kopeykin <kopeykindzzesen@gmail.com>
2020-01-29 18:40:53 +0300

    add AdCreateView and form to create ads

---
+++ bboard/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+
+from .models import Ad
+
+
+class AdForm(ModelForm):
+    class Meta:
+        model = Ad
+        fields = (
+            "title",
+            "content",
+            "price",
+            "rubric",
+        )
--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
+from .forms import AdForm
 from .models import Ad, Rubric
 
 
@@ -16,3 +19,14 @@ def by_rubric(request, rubric_id):
     current_rubric = rubrics.get(pk=rubric_id)
     context = {"ads": ads, "rubrics": rubrics, "current_rubric": current_rubric}
     return render(request, "bboard/by_rubric.html", context)
+
+
+class AdCreateView(CreateView):
+    template_name = "bboard/create.html"
+    form_class = AdForm
+    success_url = reverse_lazy("index")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["rubrics"] = Rubric.objects.all()
+        return context
